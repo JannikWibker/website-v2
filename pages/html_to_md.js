ed0607d2ec3d91ce77314a0aaa3e2e9a3af78f54
Jannik Wibker <jannikwibker@gmail.com>
2018-01-28 01:36:18 +0100

    updated html_to_md page

--- pages/html_to_md.js
+++ pages/html_to_md.js
@@ -22,7 +22,61 @@ const HtmlToMd = styled.div`
 
 const info = {
   'html_to_md': [
-    <p>test</p>
+    <p>github repo: <a href="https://github.com/JannikWibker/html_to_md">html_to_md</a></p>,
+    <p>Ever wanted to turn existing HTML into markdown? No? Yeah maybe it's not super useful but it has its use cases:
+      <ul>
+        <li>'caching' strategy for simple pages for offline usage (such as wikipedia pages or other similar pages with information such as <a href="http://www.vlsm-calc.net/models.php">this one</a> for example)</li>
+        <li>reading mode when compiling the markdown to pdf or back to html (overall removes clutter from sites)</li>
+        <li>turning html documents back into markdown incase the markdown source code was lost</li>
+      </ul>
+    </p>
+  ],
+  'features': [
+    <p><em>html_to_md</em> has support for many html elements (unsupported elements are just ignored):
+      <ul>
+        <li>spans</li>
+        <li>links (<em>a</em>)</li>
+        <li>text formatting (<em>em</em>, <em>i</em>, <em>strong</em>, <em>b</em>, <em>strike</em>)</li>
+        <li>images (<em>img</em>)</li>
+        <li>tables</li>
+        <li>headings (<em>h1</em>, <em>h2</em>, <em>h3</em>, <em>h4</em>, <em>h5</em>, <em>h6</em>)</li>
+        <li>paragraphs (<em>p</em>)</li>
+        <li>lists (<em>ul</em>, <em>ol</em>)</li>
+        <li>code</li>
+        <li>super script (<em>sup</em>)</li>
+        <li>sub script (<em>sub</em>)</li>
+        <li>linebreaks (<em>br</em>)</li>
+        <li>hr</li>
+      </ul>
+    </p>
+  ],
+  'installation': [
+    <p>Best just use <code>git</code> to install <em>html_to_md</em></p>,
+    <code>git clone https://github.com/JannikWibker/html_to_md</code>
+  ],
+  'usage': [
+    <p>import html_to_md</p>,
+    <code>const html_to_md = require('../index.js')</code>,
+    <p>example html</p>,
+    <code>{'const html = `'}<br />
+      &nbsp;&nbsp;{'<body>'}<br />
+      &nbsp;&nbsp;&nbsp;&nbsp;{'<h1>This is a H1</h1>'}<br />
+      &nbsp;&nbsp;&nbsp;&nbsp;{'<p>This is a paragraph</p>'}<br />
+      <br />
+      &nbsp;&nbsp;{'</body>`'}<br />
+    </code>,
+    <p>call html_to_md with the html and a selector (it's like document.querySelector, if none is given, <code>'body'</code> is used)</p>,
+    <code>html_to_md(html, 'body')[0]</code>,
+    <p>the <code>[0]</code> is because html_to_md returns an array (with entries for every element returned by the selector, in this case it's just one)</p>,
+    <p>The following markdown is returned:</p>,
+    <code>
+    {'# This is a H1'}<br />
+    <br />
+    {'This is a paragraph'}<br />
+    </code>
+  ],
+  'demo': [
+    <p>for a demonstration of <em>html_to_md</em>'s go visit the <a href="https://github.com/JannikWibker/html_to_md/tree/master/demo">github repository</a></p>,
   ]
 }
 
